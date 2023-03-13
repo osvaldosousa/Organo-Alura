28fc5e4c4b8122dc224ff03e39bca8facc398458
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-13 17:01:44 -0300

    função mudar cor agora mudar cor de time e background
    times recebe apenas cor usada para card e background

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,5 @@
+import hexToRgba from 'hex-to-rgba';
+
 import Colaborador from '../Colaborador'
 import './time.css'
 
@@ -8,23 +10,23 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
         className="time"
         style={{
           backgroundImage: 'url(/imagens/fundo.png)',
-          backgroundColor: time.corPrimaria
+          backgroundColor: hexToRgba(time.cor, '0.6')
         }}
       >
         <input 
           onChange={evento => mudarCor(evento.target.value, time.nome)}
-          value={time.corSecundaria}
+          value={time.cor}
           type={'color'} 
           className='input-cor'
         />
-        <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
         <div className="colaboradores">
           {colaboradores.map((colaborador, indice) => {
             return (
               <Colaborador
                 key={indice}
                 colaborador={colaborador}
-                corDeFundo={time.corSecundaria}
+                corDeFundo={time.cor}
                 aoDeletar={aoDeletar}
               />
             )
