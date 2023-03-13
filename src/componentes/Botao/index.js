1a511c858335cbcc6520a84e38eb9cdc0a0d5818
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-13 14:14:11 -0300

    clonando organo
    continuação do curso de react alura

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,9 +1,7 @@
 import './botao.css'
 
-const Botao = ({texto}) => {
-    return <button className="botao">
-        {texto}
-    </button>
+const Botao = ({ texto }) => {
+  return <button className="botao">{texto}</button>
 }
 
 export default Botao
