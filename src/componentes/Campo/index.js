c77fdcd77de3e4d3beb3ceb4feeba3c527478f68
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-14 16:21:14 -0300

    refatorando componente Campo
    atribuindo className por javascript

--- src/componentes/Campo/index.js
+++ src/componentes/Campo/index.js
@@ -1,16 +1,19 @@
-import './campo-texto.css'
+import './campo.css'
 
 const CampoTexto = ({
+  type = 'text',
   label,
   placeholder,
   valor,
   aoAlterado,
   obrigatorio = false
 }) => {
+  // const classType = type === 'color' ? 'campo -color': 'campo';
   return (
-    <div className="campo-texto">
+    <div className={`campo -${type}`}>
       <label>{label}</label>
       <input
+        type={type}
         value={valor}
         onChange={evento => aoAlterado(evento.target.value)}
         required={obrigatorio}
